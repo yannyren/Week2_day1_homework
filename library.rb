31ab0cde915a2b7d3ead25f36d1b2942c45f65c3
Guy Bachkhaznadji <guysergebach@hotmail.com>
2017-07-03 16:42:29 +0100

    all done

--- library.rb
+++ library.rb
@@ -13,9 +13,41 @@ class Library
    def book_detail(book_name)
       
       for book in books
-        if (book_name == :title)
-          return book
+        if (book_name == book[:title])
+          return books
         end
-    end
+      end
    end
+
+  def rental_details(book_name)
+
+    for book in books
+     if (book_name == book[:title])
+       return book[:rental_details]
+     end
+   end
+ end
+
+ def new_book(book_name)
+  @books << {
+        title: book_name,
+        rental_details: {
+          student_name: "",
+          due_date: ""
+        }
+      }
+ end
+
+  def update_rental_details(book_name, new_renter, new_due_date)
+
+    for book in books
+     if (book_name == book[:title])
+        book[:rental_details][:student_name] = new_renter
+        book[:rental_details][:due_date] = new_due_date
+     end
+   end
+ end
+
 end
+
+
--- library_spec.rb
+++ library_spec.rb
@@ -40,5 +40,66 @@ class LibraryTest < MiniTest::Test
         }}], fantasy_section.book_detail("lord of the rings"))
   end
 
+  def test_rental_details
+    fantasy_section = Library.new(
+      [{
+        title: "lord of the rings",
+        rental_details: {
+          student_name: "Jeff",
+          due_date: "01/12/16"
+        }
+      }
+      ])
+  assert_equal({
+          student_name: "Jeff",
+          due_date: "01/12/16"
+        }, fantasy_section.rental_details("lord of the rings"))
+  end
+
+
+  def test_new_book
+    fantasy_section = Library.new(
+      [{
+        title: "lord of the rings",
+        rental_details: {
+          student_name: "Jeff",
+          due_date: "01/12/16"
+        }
+      }
+      ])
+  assert_equal([{
+        title: "lord of the rings",
+        rental_details: {
+          student_name: "Jeff",
+          due_date: "01/12/16"
+        }
+      },
+        {
+        title: "The Hobbit",
+        rental_details: {
+          student_name: "",
+          due_date: ""
+        }
+      }
+      ], fantasy_section.new_book("The Hobbit") )
+  end
+
+  def test_update_rental_details
+    fantasy_section = Library.new(
+      [{
+        title: "lord of the rings",
+        rental_details: {
+          student_name: "Jeff",
+          due_date: "01/12/16"
+        }
+      }
+      ])
+  assert_equal([{
+        title: "lord of the rings",
+        rental_details: {
+          student_name: "Guy",
+          due_date: "01/08/17"
+        }}], fantasy_section.update_rental_details("lord of the rings", "Guy", "01/08/17"))
+  end
 
 end
